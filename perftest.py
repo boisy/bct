a5bf4d0e078d87d1c316c88dcba017cb3bdb2d89
Boisy Pitre <boisy@toughmac.com>
2018-12-05 10:19:56 -0600

    Optimized code

--- perftest.py
+++ perftest.py
@@ -6,40 +6,7 @@ import numpy
 
 import bct
 
-#iterations = 10000
-#for n in range(1, iterations):
-#	n1 = bct.unary_SNG(4, 16, .5)
-
-#end = time.time()
-#print(iterations, "X -- unary_SNG(4, 16, .5) =", end - start)
-
-#start = time.time()
-
-#iterations = 1000
-#for n in range(1, iterations):
-#	n1 = bct.lfsr_SNG(4, 16, .5)
-
-#end = time.time()
-#print(iterations, "X -- unary_SNG(4, 16, .5) =", end - start)
-
-
-def multiply(n1, n2):
-	result = bct.and_op(n1, n2)
-	return result
-
-# Pass 1
-# 1110 
-# 0101
-
-# Pass 2 - AND and check result
-# 1110 1110 1110 1110
-# 0000 1111 0000 1111 
-# ------------------- <- AND
-# 0000 1110 0000 1110 <- evaluate in chunks step by step (0/0, 3/8, 3/12, 6/16)
-
 # Use substreams to minimize the impact of the bitstreams on RAM, and at the same time, compute them piecemeal to see if they fall within our desired accuracy.
- 
-
 class bctTest(unittest.TestCase):
 	def test_multiply_two_bitstreams(self):
 		# Measure multiplication of two bit streams
@@ -52,10 +19,13 @@ class bctTest(unittest.TestCase):
 
 		term1 = .5
 		term2 = .5
+		true_result = term1 * term2
 		n1 = bct.unary_SNG(precision, bitstream_length, term1)
 		n2 = bct.lfsr_SNG(precision, bitstream_length, term2)
 
-		print("n1 = ", n1, ", n2 = ", n2)
+		print("Multiply ", n1, "by", n2)
+		print("True result is", true_result)
+		print("=====================================================")
 
 		for part in range(1, bitstream_length + 1):
 			cdn1 = numpy.zeros(0)
@@ -63,26 +33,28 @@ class bctTest(unittest.TestCase):
 			major = bitstream_length * (part - 1)
 			for i in range(1, bitstream_length + 1):
 				offset = major + i
-				print(offset)
+#				print(offset)
 				cdn1 = numpy.append(cdn1, bct.clockdiv_bit(1, n1, 2, offset))
 				cdn2 = numpy.append(cdn2, bct.clockdiv_bit(2, n2, 2, offset))
 
-			print("cdn1 = ", cdn1)
-			print("cdn2 = ", cdn2)
+			print("term 1 =", cdn1)
+			print("term 2 =", cdn2)
 	
 			cdn1Xcdn2 = bct.and_op(cdn1, cdn2)
-			print("cdn1Xcdn2 = cdn1 x cdn2 = ", cdn1Xcdn2)
+			print("term 1 X term 2 =", cdn1Xcdn2)
 
 			accumulated_result = accumulated_result + bct.number_of_1(cdn1Xcdn2)
 			accumulated_result_length += bitstream_length
 			result_float = accumulated_result / accumulated_result_length
 
-			true_result = term1 * term2
 			error = abs(result_float - true_result)
 			print("true_result = ", true_result, ", result_float = ", result_float, ", error = ", error)
 			if error <= epsilon:
-				print("result is within error.")
+				print("result is within error after", accumulated_result_length, "bits.")
 				return
+			else:
+				print("not accurate enough... try with", bitstream_length, "more bits.")
+	
 		
 # perform unit testing if no parameters specified (e.g. python bct.py)
 if __name__ == '__main__':
