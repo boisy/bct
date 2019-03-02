ef1e415f184fe55e0dcfb38ff08fcb43dbf07af0
Boisy Pitre <boisy.pitre@tee-boy.com>
2019-03-02 17:03:34 -0600

    Update

--- perftest.py
+++ perftest.py
@@ -15,35 +15,16 @@ import bct
 class bctTest(unittest.TestCase):
 	def test_main(self):
 		f = open("results.csv", "w+")
-		f.write("sngs,methods_conventional,methods_segmented,precision,bitstream length, segment length,mae,time (conventional),time (segmented)\n")
+		f.write("total time,sngs,methods_conventional,methods_segmented,precision,bitstream length, segment length,mae,time (conventional),time (segmented),improvement\n")
 		f.close()
 
-		sngs = [bct.unary_SNG, bct.unary_SNG]
-		methods_conventional = [bct.clockdiv, bct.clockdiv]
-		methods_segmented = [bct.clockdiv_bits, bct.clockdiv_bits]
-		methods_conventional = [bct.rotate, bct.rotate]
-		methods_segmented = [bct.rotate_bits, bct.rotate_bits]
-		bctTest.multiply_test_comprehensive_2_terms(self, 4, sngs, methods_conventional, methods_segmented, 4, 0.0)
-
-		sngs = [bct.sobol_SNG, bct.sobol_SNG]
-		methods_conventional = [bct.rotate, bct.rotate]
-		methods_segmented = [bct.rotate_bits, bct.rotate_bits]
-		bctTest.multiply_test_comprehensive_2_terms(self, 4, sngs, methods_conventional, methods_segmented, 4, 0.0)
-
-		sngs = [bct.sobol_SNG, bct.sobol_SNG]
-		methods_conventional = [bct.rotate, bct.rotate]
-		methods_segmented = [bct.rotate_bits, bct.rotate_bits]
-		bctTest.multiply_test_comprehensive_2_terms(self, 4, sngs, methods_conventional, methods_segmented, 4, 0.01)
-
-		sngs = [bct.sobol_SNG, bct.sobol_SNG]
-		methods_conventional = [bct.rotate, bct.rotate]
-		methods_segmented = [bct.rotate_bits, bct.rotate_bits]
-		bctTest.multiply_test_comprehensive_2_terms(self, 4, sngs, methods_conventional, methods_segmented, 4, 0.05)
-
-		sngs = [bct.sobol_SNG, bct.sobol_SNG]
-		methods_conventional = [bct.rotate, bct.rotate]
-		methods_segmented = [bct.rotate_bits, bct.rotate_bits]
-		bctTest.multiply_test_comprehensive_2_terms(self, 4, sngs, methods_conventional, methods_segmented, 4, 0.1)
+		segment_length = 1
+		for mae in [0.0]:
+			precision = 8
+			sngs = [bct.sobol_SNG, bct.sobol_SNG, bct.sobol_SNG]
+			methods_conventional = [bct.rotate, bct.rotate, bct.rotate]
+			methods_segmented = [bct.rotate_bits, bct.rotate_bits, bct.rotate_bits]
+			bctTest.multiply_test_comprehensive_3_terms(self, precision, sngs, methods_conventional, methods_segmented, segment_length, mae)
 
 	def multiply_test_comprehensive_2_terms(self, precision, sngs, methods_conventional, methods_segmented, segment_length = 4, mae = 0.0):
 		logger = logging.getLogger(__name__)	
@@ -77,7 +58,7 @@ class bctTest(unittest.TestCase):
 		for s in methods_segmented:
 			methods_segmented_names = methods_segmented_names + s.__name__ + " "
 
-		f.write(sng_names + "," + methods_conventional_names + "," + methods_segmented_names + "," + str(precision) + "," + str(bitstream_length) + "," + str(segment_length) + "," + str(mae) + "," + str(total_conventional_time) + "," + str(total_segmented_time) + "\n")
+		f.write(str(total_segmented_time + total_conventional_time) + "," + sng_names + "," + methods_conventional_names + "," + methods_segmented_names + "," + str(precision) + "," + str(bitstream_length) + "," + str(segment_length) + "," + str(mae) + "," + str(total_conventional_time) + "," + str(total_segmented_time) + "," + str((1 / (total_segmented_time / total_conventional_time))) + "\n")
 		f.close()
 
 	def multiply_test_comprehensive_3_terms(self, precision, sngs, methods_conventional, methods_segmented, segment_length = 4, mae = 0.0):
@@ -99,6 +80,23 @@ class bctTest(unittest.TestCase):
 					total_segmented_time = total_segmented_time + times[1]
 					logger.critical('total conventional time = %f', total_conventional_time)
 					logger.critical('total segmented time = %f', total_segmented_time)
+
+		f = open("results.csv", "a+")
+
+		sng_names = ""
+		for s in sngs:
+			sng_names = sng_names + s.__name__ + " "
+
+		methods_conventional_names = ""
+		for s in methods_conventional:
+			methods_conventional_names = methods_conventional_names + s.__name__ + " "
+
+		methods_segmented_names = ""
+		for s in methods_segmented:
+			methods_segmented_names = methods_segmented_names + s.__name__ + " "
+
+		f.write(str(total_segmented_time + total_conventional_time) + "," + sng_names + "," + methods_conventional_names + "," + methods_segmented_names + "," + str(precision) + "," + str(bitstream_length) + "," + str(segment_length) + "," + str(mae) + "," + str(total_conventional_time) + "," + str(total_segmented_time) + "," + str((1 / (total_segmented_time / total_conventional_time))) + "\n")
+		f.close()
 
 	def multiply_bitstreams(self, sngs, methods_conventional, methods_segmented, terms, precision=4, bitstream_length=16, segment_length=4, mae = 0.0, run_loops=10):
 		logger = logging.getLogger(__name__)	
@@ -199,14 +197,15 @@ class bctTest(unittest.TestCase):
 			accumulated_result_length += segment_length
 			result_float = accumulated_result / accumulated_result_length
 
-			error = abs(result_float - true_result)
-			logger.info("True result = %f, result_float = %f (%d/%d), error = %f", true_result, result_float, accumulated_result, accumulated_result_length, error)
-			if error <= mae:
-				logger.error("True result = %f, result_float = %f (%d/%d), error = %f", true_result, result_float, accumulated_result, accumulated_result_length, error)
-				logger.error("result is within error after %d bits (%d steps)", accumulated_result_length, int(accumulated_result_length / segment_length))
-				return
-			else:
-				logger.error("not accurate enough with %d bits", accumulated_result_length)
+			if mae != 0.0:
+				error = abs(result_float - true_result)
+				logger.info("True result = %f, result_float = %f (%d/%d), error = %f", true_result, result_float, accumulated_result, accumulated_result_length, error)
+				if error <= mae:
+					logger.error("True result = %f, result_float = %f (%d/%d), error = %f", true_result, result_float, accumulated_result, accumulated_result_length, error)
+					logger.error("result is within error after %d bits (%d steps)", accumulated_result_length, int(accumulated_result_length / segment_length))
+					return
+				else:
+					logger.error("not accurate enough with %d bits", accumulated_result_length)
 
 
 # perform unit testing if no parameters specified (e.g. python bct.py)
